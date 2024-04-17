54127088145ff1ff802a132bf05672b6b083b2a9
PhantomZoro <phaneendrab.iitm@outlook.com>
2024-04-17 19:50:03 +0530

    First push

---
+++ namaste-react/App.js
@@ -0,0 +1,23 @@
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement(
+        "div",
+        {id:"child1"},
+        [React.createElement("h1",{},"I am H1 tag"),
+        React.createElement("h2",{},"I am H2 tag")]
+    ),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1",{},"I am H3 tag"),
+        React.createElement("h2",{},"I am H4 tag")]
+    )]
+);
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root"));
+
+root.render(parent);
+console.log(parent);
